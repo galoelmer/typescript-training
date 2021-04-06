b45c1b29c19cf83f7a543455ad79cbbf16b203ec
Elmer C. Galo <elmer.galo@outlook.com>
2021-04-06 19:54:02 -0400

    Fetch user input

--- drag-and-drop-project/src/app.ts
+++ drag-and-drop-project/src/app.ts
@@ -46,10 +46,37 @@ class ProjectInput {
     this.attach();
   }
 
+  private clearInputs() {
+    this.titleElement.value = "";
+    this.descriptionElement.value = "";
+    this.peopleElement.value = "";
+  }
+
+  private gatherUserInput(): [string, string, number] | void {
+    const inputTitle = this.titleElement.value;
+    const inputDescription = this.descriptionElement.value;
+    const inputPeople = this.peopleElement.value;
+
+    if (
+      inputTitle.trim().length === 0 ||
+      inputDescription.trim().length === 0 ||
+      inputPeople.trim().length === 0
+    ) {
+      alert("Invalid input, try again");
+      return;
+    } else {
+      return [inputTitle, inputDescription, +inputPeople];
+    }
+  }
+
   @AutoBind
   private submitHandler(event: Event) {
     event.preventDefault();
-    console.log(this.titleElement.value);
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      this.clearInputs();
+      console.log(userInput);
+    }
   }
 
   private configure() {
