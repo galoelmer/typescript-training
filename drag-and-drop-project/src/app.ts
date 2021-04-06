296fcca97ec340c6aaed3e4f3f635b66216cf10e
Elmer C. Galo <elmer.galo@outlook.com>
2021-04-06 17:39:14 -0400

    Interacting with DOM elements

--- drag-and-drop-project/src/app.ts
+++ drag-and-drop-project/src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleElement: HTMLInputElement;
+  descriptionElement: HTMLInputElement;
+  peopleElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -14,10 +17,32 @@ class ProjectInput {
       true
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+
+    this.titleElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    this.configure();
     this.attach();
   }
 
-  attach() {
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
+  }
+
+  private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
 }
