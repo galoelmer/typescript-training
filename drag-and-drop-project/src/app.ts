b336e3e3258c1c2842c3854224544dc1dc85b096
Elmer C. Galo <elmer.galo@outlook.com>
2021-04-07 00:58:40 -0400

    Render project list template to DOM

--- drag-and-drop-project/src/app.ts
+++ drag-and-drop-project/src/app.ts
@@ -56,6 +56,41 @@ function AutoBind(_: any, __: string, descriptor: PropertyDescriptor) {
   return adjustedDescriptor;
 }
 
+/* Porject List Class */
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: "active" | "finished") {
+    this.templateElement = document.getElementById(
+      "project-list"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent =
+      this.type.toUpperCase() + " PROJECTS";
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
+
+/* Project Input Class */
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -152,3 +187,5 @@ class ProjectInput {
 }
 
 const newProject = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
